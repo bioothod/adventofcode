15dfef66feb5679b640b50d87aacb5e7d6968fd7
Evgeniy Polyakov <zbr@ioremap.net>
2024-12-03 03:17:36 +0000

    Day02.2

--- 2024/src/Day02.hs
+++ 2024/src/Day02.hs
@@ -6,32 +6,44 @@ num_words :: [String] -> [Integer]
 num_words [] = []
 num_words ls = map read ls
 
+max_step :: Integer
 max_step = 3
+min_step :: Integer
 min_step = 1
-check_pair_decr :: Integer -> Integer -> Bool
-check_pair_decr a b = b <= a - min_step && b >= a - max_step
+check_pair_decr :: (Integer, Integer) -> Bool
+check_pair_decr (a, b) = b <= a - min_step && b >= a - max_step
 
-check_pair_acc :: (Integer, Bool) -> Integer -> (Integer, Bool)
-check_pair_acc (acc_value, acc_bool) next = (next, bool_result)
-  where bool_result = (&&) acc_bool $ check_pair_decr acc_value next
+check_pairs_acc :: [Integer] -> [Bool]
+check_pairs_acc xs = True : (map check_pair_decr . zip xs $ drop 1 xs)
 
+try_and_drop :: Int -> [Integer] -> Bool
+try_and_drop x xs = let (left, right) = splitAt x xs
+                        new_list = left ++ (drop 1 right)
+                    in and $ check_pairs_acc new_list
 
-is_safe_decr :: [Integer] -> Bool
-is_safe_decr [] = False
-is_safe_decr ls = bool_result
- where (_, bool_result) = foldl check_pair_acc (start, True) rest
-        where ([start], rest) = splitAt 1 ls
+find_one_dropped :: [Bool] -> [Integer] -> Bool
+find_one_dropped _bools xs = or $ map (\x -> try_and_drop x xs) [0..(length xs)-1]
 
-is_safe :: [Integer] -> Bool
-is_safe [] = False
-is_safe report = (is_safe_decr report) || (is_safe_decr $ reverse report)
+drop_one_and_check :: Bool -> [Integer] -> Bool
+drop_one_and_check strict xs = let bools = check_pairs_acc xs
+                                   num_failures = length $ filter (not) bools
+                               in case num_failures of
+                                    0 -> True
+                                    _ -> if strict
+                                         then False
+                                         else find_one_dropped bools xs
+
+is_safe_drop :: Bool -> [Integer] -> Bool
+is_safe_drop strict report = (drop_one_and_check strict report) || (drop_one_and_check strict  $ reverse report)
 
 solve1 :: String -> Solution
 solve1 input = do
   let reports = map num_words . map words $ lines input
-  let safe = filter is_safe reports
+  let safe = filter (is_safe_drop True) reports
   toInteger $ length safe
 
 solve2 :: String -> Solution
 solve2 input = do
-  2
+  let reports = map num_words . map words $ lines input
+  let safe = filter (is_safe_drop False) reports
+  toInteger $ length safe
