766b1eb18b838c553c44d9e0d5f1e23a6fc70a3c
Evgeniy Polyakov <zbr@ioremap.net>
2025-12-06 14:00:30 +0000

    2025: use common utils

--- 2025/src/02/solution.cpp
+++ 2025/src/02/solution.cpp
@@ -7,57 +7,20 @@
 
 #include "utils.hpp"
 
-unsigned long long quick_pow10(int n)
-{
-  static std::vector<unsigned long long> pow10 = {
-    1, 10, 100, 1000, 10000,
-    100000, 1000000, 10000000, 100000000, 1000000000,
-    10000000000, 100000000000, 1000000000000, 10000000000000,
-    100000000000000, 1000000000000000, 10000000000000000, 100000000000000000,
-  };
-
-  if ((size_t)n >= pow10.size()) {
-    throw std::invalid_argument(std::format("quick_pow10({}): n is too big, max is {}", n, pow10.size()));
-  }
-
-  return pow10[n];
-}
-
-template <std::integral T>
-T combine(const std::vector<T> &value) {
-  T ret = 0;
-  for (auto r = value.begin(); r != value.end(); ++r) {
-    ret *= 10;
-    ret += *r;
-  }
-  return ret;
-}
-template <std::integral T>
-std::vector<T> split(T value) {
-  std::vector<T> tmp;
-  while (value > 0) {
-    T r = value % 10;
-    tmp.push_back(r);
-    value /= 10;
-  }
-  std::reverse(tmp.begin(), tmp.end());
-  return tmp;
-}
-
 template <std::integral T>
 std::tuple<T, T> split_and_pad(T value) {
-  auto tmp = split(value);
+  auto tmp = aoc::split(value);
 
   if (tmp.size() % 2 != 0) {
-    tmp = split(quick_pow10(tmp.size()+1));
+    tmp = aoc::split(aoc::quick_pow10(tmp.size()+1));
   }
 
   T middle = tmp.size() / 2;
   std::vector<T> v0(tmp.begin(), tmp.begin() + middle);
   std::vector<T> v1(tmp.begin()+middle, tmp.end());
 
-  T f0 = combine(v0);
-  T f1 = combine(v1);
+  T f0 = aoc::combine(v0);
+  T f1 = aoc::combine(v1);
 
   return {f0, f1};
 }
@@ -67,7 +30,7 @@ unsigned long long solve_part1(const std::vector<std::pair<unsigned long long, u
   for (auto ids: id_ranges) {
     auto [start, end] = ids;
     auto [f0, f1] = split_and_pad(start);
-    unsigned long long mult = quick_pow10(split(f0).size());
+    unsigned long long mult = aoc::quick_pow10(aoc::split(f0).size());
 
     if (f0 != f1) {
       if (f0 > f1) {
@@ -86,7 +49,7 @@ unsigned long long solve_part1(const std::vector<std::pair<unsigned long long, u
       f1 += 1;
 
       if (f0 % 10 == 0) {
-        mult = quick_pow10(split(f0).size());
+        mult = aoc::quick_pow10(aoc::split(f0).size());
       }
 
       combined = f0 * mult + f1;
@@ -119,7 +82,7 @@ unsigned long long solve_part2(const std::vector<std::pair<unsigned long long, u
   for (auto ids: id_ranges) {
     auto [start, end] = ids;
     for (auto index=start; index<=end; ++index) {
-      auto spl = split(index);
+      auto spl = aoc::split(index);
       for (size_t n =1; n<spl.size()/2+1; ++n) {
         if (check_n(spl, n)) {
           sum += index;
